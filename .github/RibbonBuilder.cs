aee8d69d29247bdce07eaff2988dc888617aec8c
Shawqih <shwaqi124@gmail.com>
2026-03-08 03:30:56 +0300

    Add files via upload

---
+++ .github/RibbonBuilder.cs
@@ -0,0 +1,11 @@
+
+namespace MEGAPLAN.UI
+{
+    public class RibbonBuilder
+    {
+        public static void CreateRibbon()
+        {
+            // Ribbon creation placeholder
+        }
+    }
+}
